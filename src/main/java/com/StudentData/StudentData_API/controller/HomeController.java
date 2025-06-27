7fe9250ccd9b8104464b19c2c17d7717f2681d96
Raunak Ranjan <raunakranjann@gmail.com>
2025-06-27 16:19:40 +0530

    Adding HomePage using thymeleaf

---
+++ src/main/java/com/StudentData/StudentData_API/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.StudentData.StudentData_API.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "index.html";
+    }
+}
